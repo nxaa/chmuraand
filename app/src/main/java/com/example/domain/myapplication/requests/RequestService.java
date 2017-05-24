313181ee86dfa0d80c11f05868bd849ea9543269
kuba12kozak <kuba12kozak@gmail.com>
2017-05-25 01:46:30 +0200

    metoda to postowania plikow

--- app/src/main/java/com/example/domain/myapplication/requests/RequestService.java
+++ app/src/main/java/com/example/domain/myapplication/requests/RequestService.java
@@ -5,6 +5,9 @@ import android.os.AsyncTask;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -12,6 +15,9 @@ import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 /**
  * Created by KOZAK on 24.05.2017.
@@ -19,43 +25,120 @@ import java.net.URL;
 
 public class RequestService {
     private static String TRIPS_URL_BASE = "http://tripper-api.azurewebsites.net/trips/";
-/*    public String postMedia(String tripId, String m_id) {
-        URL url;
+
+    public String postMedia(String tripId, String m_id, String filepath, String filefield, String fileMimeType) {
         HttpURLConnection connection = null;
-        String output = "";
+        DataOutputStream outputStream = null;
+        InputStream inputStream = null;
+        Map<String, String> params = new HashMap<>();
+
+        params.put("m_id", m_id);
+        String twoHyphens = "--";
+        String boundary = "*****" + Long.toString(System.currentTimeMillis()) + "*****";
+        String lineEnd = "\r\n";
+
+        String result = "";
+
+        int bytesRead, bytesAvailable, bufferSize;
+        byte[] buffer;
+        int maxBufferSize = 1 * 1024 * 1024;
+
+        String[] q = filepath.split("/");
+        int idx = q.length - 1;
+
         try {
-            url = new URL(TRIPS_URL_BASE + tripId + "/media");
+            File file = new File(filepath);
+            FileInputStream fileInputStream = new FileInputStream(file);
+
+            URL url = new URL(TRIPS_URL_BASE + tripId + "/media");
             connection = (HttpURLConnection) url.openConnection();
-            connection.setUseCaches(false);
+
+            connection.setDoInput(true);
             connection.setDoOutput(true);
+            connection.setUseCaches(false);
 
             connection.setRequestMethod("POST");
             connection.setRequestProperty("Connection", "Keep-Alive");
-            connection.setRequestProperty("Content-Type", "");
+            connection.setRequestProperty("User-Agent", "Android Multipart HTTP Client 1.0");
+            connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
 
-            int responseCode = connection.getResponseCode();
-            if (responseCode != 200) {
-                return "Error bang, response code = " + responseCode;
+            outputStream = new DataOutputStream(connection.getOutputStream());
+            outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+            outputStream.writeBytes("Content-Disposition: form-data; name=\"" + filefield + "\"; filename=\"" + q[idx] + "\"" + lineEnd);
+            outputStream.writeBytes("Content-Type: " + fileMimeType + lineEnd);
+            outputStream.writeBytes("Content-Transfer-Encoding: binary" + lineEnd);
+
+            outputStream.writeBytes(lineEnd);
+
+            bytesAvailable = fileInputStream.available();
+            bufferSize = Math.min(bytesAvailable, maxBufferSize);
+            buffer = new byte[bufferSize];
+
+            bytesRead = fileInputStream.read(buffer, 0, bufferSize);
+            while (bytesRead > 0) {
+                outputStream.write(buffer, 0, bufferSize);
+                bytesAvailable = fileInputStream.available();
+                bufferSize = Math.min(bytesAvailable, maxBufferSize);
+                bytesRead = fileInputStream.read(buffer, 0, bufferSize);
             }
 
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            outputStream.writeBytes(lineEnd);
 
-            String a;
-            while ((a = bufferedReader.readLine()) != null) {
-                output += a;
+            // Upload POST Data
+            Iterator<String> keys = params.keySet().iterator();
+            while (keys.hasNext()) {
+                String key = keys.next();
+                String value = params.get(key);
+
+                outputStream.writeBytes(twoHyphens + boundary + lineEnd);
+                outputStream.writeBytes("Content-Disposition: form-data; name=\"" + key + "\"" + lineEnd);
+                outputStream.writeBytes("Content-Type: text/plain" + lineEnd);
+                outputStream.writeBytes(lineEnd);
+                outputStream.writeBytes(value);
+                outputStream.writeBytes(lineEnd);
             }
 
+            outputStream.writeBytes(twoHyphens + boundary + twoHyphens + lineEnd);
 
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
+            if (200 != connection.getResponseCode()) {
+                return "Error, response code = " + connection.getResponseCode();
+            }
+
+            inputStream = connection.getInputStream();
+
+            result = this.convertStreamToString(inputStream);
+
+            fileInputStream.close();
+            inputStream.close();
+            outputStream.flush();
+            outputStream.close();
+
+            return result;
+        } catch (Exception e) {
+            return "Error";
+        }
+    }
+
+    private String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
-            connection.disconnect();
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
-
-        return output;
-    }*/
+        return sb.toString();
+    }
 
     public String getPresentations(String tripId) {
         URL url;
